1b14e6f8d2b9a669d54151a7746e41df71de4ee9
Alexandr Baranov <a.81.4@yandex.ru>
2024-05-27 05:47:54 +0500

    добавлена директория с данными бэка

--- utils/strings.py
+++ utils/strings.py
@@ -2,11 +2,11 @@
 
 
 # Пути. Суффикс _path
-costs_order_path = 'Приказ_о_стоимости_обучения_аспирантов_на_2024_2025_учебный_год.pdf'
-military_order_path = 'Приказ_Об_организации_воинского_учета_граждан,_в_т_ч_бронировнаия.PDF'
-students_list_path = 'Списки аспирантов.xlsx'
-existing_notification_path = 'existing_notification.csv'
-active_users_path = 'active_users.csv'
+costs_order_path = r'info_docs/Приказ_о_стоимости_обучения_аспирантов_на_2024_2025_учебный_год.pdf'
+military_order_path = r'info_docs/Приказ_Об_организации_воинского_учета_граждан,_в_т_ч_бронировнаия.PDF'
+students_list_path = r'info_docs/Списки аспирантов.xlsx'
+existing_notification_path = r'backend_data/existing_notification.csv'
+active_users_path = r'backend_data/active_users.csv'
     
 # Сообщения от бота. Суффикс _msg
 welcome_msg = 'Приветствуем вас в «Нейротьюторе» — умном боте, который поможет аспирантам в обучении!'
